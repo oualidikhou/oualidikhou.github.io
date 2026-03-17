a1774ba2568cb9110e0f9b95cb4782a8367c3afa
oualidikhou <oualid.ikhou@outlook.fr>
2026-03-17 21:05:22 +0100

    Update fetch_scholar.py

--- scripts/fetch_scholar.py
+++ scripts/fetch_scholar.py
@@ -3,20 +3,26 @@ import json
 
 scholar_id = "n26K8XoAAAAJ"
 
-author = scholarly.search_author_id(scholar_id)
-author = scholarly.fill(author, sections=['publications'])
+try:
+    author = scholarly.search_author_id(scholar_id)
+    author = scholarly.fill(author, sections=['publications'])
 
-publications = []
+    publications = []
 
-for pub in author['publications']:
-    bib = pub['bib']
+    for pub in author['publications']:
+        bib = pub['bib']
+        publications.append({
+            "title": bib.get("title",""),
+            "authors": bib.get("author",""),
+            "venue": bib.get("venue",""),
+            "year": bib.get("pub_year","")
+        })
 
-    publications.append({
-        "title": bib.get("title",""),
-        "authors": bib.get("author",""),
-        "venue": bib.get("venue",""),
-        "year": bib.get("pub_year","")
-    })
+    with open("_data/publications.json","w") as f:
+        json.dump(publications,f,indent=2)
 
-with open("_data/publications.json","w") as f:
-    json.dump(publications,f,indent=2)
+except Exception as e:
+    print("Error fetching Scholar data:", e)
+    # créer un fichier vide pour ne pas bloquer Jekyll
+    with open("_data/publications.json","w") as f:
+        json.dump([],f)
